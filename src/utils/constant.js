4cd7abfe1fee66b6f739b44476b4e82f327c28fd
shivangshekhar2807 <shivangshekhar302@gmail.com>
2025-09-29 17:21:37 +0530

    made base url dynamic

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,10 @@
 
 // export const BASE_URL = "http://localhost:5555";
 
-export const BASE_URL = "/api";
+// export const BASE_URL = "/api";
+
+// src/utils/constant.js
+// export const BASE_URL = "https://developer-tinder.xyz/api";
+
+
+export const BASE_URL=location.hostname==="localhost"?"http://localhost:5555":"/api"
